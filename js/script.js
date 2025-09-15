9a8d32f0b98aecd769555fbf4d4ae45b777964d9
EdusysPrabhudutta <edusysprabhudutta@gmail.com>
2025-09-15 16:36:07 +0530

    Update

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// <script src="/lib/jquery/jquery.min.js"></script>
+// <script src="/lib/bootstrap/bootstrap.min.js"></script>
+// Preloader js
+$(window).on("load", function () {
+  "use strict";
+  $(".preloader").fadeOut(0);
+});
+
+(function ($) {
+  "use strict";
+  $(".code-tabs ul.nav-tabs").each(function () {
+    $(this).find("li:first").addClass("active");
+  });
+
+  $(".code-tabs .tab-content").each(function () {
+    $(this).find("div:first").addClass("active");
+  });
+
+  $(".accordion-header").on("click", function () {
+    let that = $(this);
+    $(this)
+      .closest(".accordion")
+      .find(".accordion-item .accordion-header")
+      .each(function () {
+        if ($(this).is(that)) {
+          if ($(this).hasClass("active")) {
+            $(this).addClass("collapsed").removeClass("active");
+          } else {
+            $(this).addClass("active").removeClass("collapsed");
+          }
+        } else {
+          if ($(this).hasClass("active")) {
+            $(this).addClass("collapsed").removeClass("active");
+          }
+        }
+      });
+  });
+})(jQuery);
